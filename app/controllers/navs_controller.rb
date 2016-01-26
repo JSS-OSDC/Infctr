dbbde0c95030af70e11da33ec8dcc9c46d316a93
shlok007 <shlok.srivastava15@gmail.com>
2016-01-26 21:44:45 +0530

    Updated views

--- app/controllers/navs_controller.rb
+++ app/controllers/navs_controller.rb
@@ -1,6 +1,7 @@
 class NavsController < ApplicationController
   def home
   	@i=1
+  	@user=current_user
   end
 
   def blog
@@ -8,4 +9,8 @@ class NavsController < ApplicationController
 
   def about_us
   end
+  def follow(notifid)
+  	Follow.create(email:current_user.email,notif_id:notifid)
+  end
+  helper_method :follow
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125153557) do
+ActiveRecord::Schema.define(version: 20160126070642) do
+
+  create_table "follows", force: :cascade do |t|
+    t.string   "email"
+    t.integer  "notif_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "notifs", force: :cascade do |t|
     t.datetime "created_at",  null: false
