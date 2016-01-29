51017c651e7fe5ce74bf2ac21df7aaccddc50593
Shlok Srivastava <shlok.srivastava15@gmail.com>
2016-01-29 15:10:10 +0000

    Added name and admission no. fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126070642) do
+ActiveRecord::Schema.define(version: 20160127191535) do
 
   create_table "follows", force: :cascade do |t|
     t.string   "email"
@@ -43,6 +43,9 @@ ActiveRecord::Schema.define(version: 20160126070642) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "admission_no"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
